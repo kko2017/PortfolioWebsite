78a90636bb398c34c6facb5013679541e44619a4
Alex KwangEun Oh <kwangeunoh1@gmail.com>
2021-03-01 15:07:46 -0400

    🛠 JS: Move to targeted navbar

--- main.js
+++ main.js
@@ -1,9 +1,62 @@
 'use strict'
 
-// When clicking a button, scrolling into the view
+// Lectangle moves to the targeted navbar when scrolling the bar. 
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact',
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+};
+
+const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            if (entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1;
+            } else {
+                selectedNavIndex = index - 1;
+            }
+        }
+     });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+    if (window.scrollY === 0) {
+        selectedNavIndex = 0;
+    } else if (window.scrollY + window.innerHeight >= document.body.clientHeight) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
+
+// Global function: scrolling into the view when clicking a button
 function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
 // Handle scrolling when tapping on the navbar menu
@@ -14,12 +67,6 @@ navbarMenu.addEventListener('click', (e) => {
     if (link == null) {
         return;
     }
-    const activeMenu = navbarMenu.querySelector('.active');
-    if (activeMenu != null) {
-        activeMenu.classList.remove('active');
-    }
-
-    target.classList.add('active');
     scrollIntoView(link);
 });
 
